b78ad79a5c276d3d003f0b9daad7db021939d4fb
Ja5onHoffman <jasonandrewhoffman@gmail.com>
2014-02-26 08:03:50 -0500

    Fix FA call. Makes basic call, sends to DB and displays result

--- client/mRoute.js
+++ client/mRoute.js
@@ -1,23 +1,31 @@
 FiledRoutes = new Meteor.Collection('filedRoutes');
 
+Template.routesTemplate.helpers({
+	filedRoutes: function() {
+		return FiledRoutes.find();
+	}
+});
 
-Template.routesTemplate.filedRoutes = function() {
-	return FiledRoutes.find();
-};
+Deps.autorun(function() {
+	Meteor.subscribe('filedRoutes', Session.get('origin'), Session.get('destination'));
+})
 
 Template.airportForm.events({
-	"click button": function(event) {
+	"submit form": function(event) {
 		event.preventDefault()
 		var origin = $('#origin').val();
-		var destination = $('#destination').val();
-		Meteor.call("callFltAware", origin, destination, function (e,result) {
+		var destination = $('#destination').val()
+		Session.set('origin', $('#origin').val());
+		Session.set('destination', $('#destination').val());
+		Meteor.call("callFltAware", origin, destination, function (e, result) {
 			if (!e && result) {
-				console.log(result.data.RoutesBetweenAirportsResult.data);
-
+				console.log(result.data.RoutesBetweenAirportsExResult.data);
 			}
 		});
 	}
 })
+
+
 
 // Push routes to database and show long list of previously filed routes. 
 
--- server/mRoute.js
+++ server/mRoute.js
@@ -1,11 +1,14 @@
 FiledRoutes = new Meteor.Collection('filedRoutes');
 
+Meteor.publish('filedRoutes', function(origin, destination) {
+	return FiledRoutes.find({origin: origin, destination: destination});
+})
 
 Meteor.startup(function() {
 	// code to run on server at startup
 });
 
-var url = "http://flightxml.flightaware.com/json/FlightXML2/RoutesBetweenAirports";
+var url = "http://flightxml.flightaware.com/json/FlightXML2/";
 var username = "jasonandrewhoffman";
 var apiKey = "323da49e858c3d25fa589d1f6de62a96807f9dab";
 
@@ -13,9 +16,26 @@ Meteor.methods({
 	callFltAware: function(origin, destination) {
 		this.unblock()
       try {
-        var result = HTTP.call("GET", url + "?origin=" + origin + "&destination=" + destination, {
-    		auth: "jasonandrewhoffman:0560af20db89ba4b2fba0dbae9d4543e36cfc69a", 
+        var result = HTTP.call("GET", url + 'RoutesBetweenAirportsEx', {
+    		auth: "jasonandrewhoffman:0560af20db89ba4b2fba0dbae9d4543e36cfc69a",
+            params: {
+                origin: origin, 
+                destination: destination,
+                howMany: 15,
+                offset: 0,
+                maxDepartureAge: "10 days",
+                maxFileAge: "30 days"
+            }
     });
+        var r = result.data.RoutesBetweenAirportsExResult.data; 
+        for (var i = 0; i < r.length; i++) {
+        	var route = {
+        		origin: origin,
+        		destination: destination,
+        		route: r[i].route
+        	}
+        	FiledRoutes.insert(route)
+        }
         return result
       } catch (e) {
         console && console.log && console.log('Exception calling', url)
