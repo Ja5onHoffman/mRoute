548fb4b8f4ab2580abda2514399801c83e596cca
Ja5onHoffman <jasonandrewhoffman@gmail.com>
2014-02-27 06:38:19 -0500

    Fix CSS add default instruction message

--- client/mRoute.js
+++ client/mRoute.js
@@ -1,5 +1,8 @@
 FiledRoutes = new Meteor.Collection('filedRoutes');
 
+Session.setDefault("origin", "default");
+Session.setDefault("destination", "default");
+
 Template.routesTemplate.helpers({
 	filedRoutes: function() {
 		return FiledRoutes.find();
@@ -25,35 +28,3 @@ Template.airportForm.events({
 	}
 })
 
-
-
-// Push routes to database and show long list of previously filed routes. 
-
-
-/* 
-
-var http = require('http');
-var rest = require('restler');
-
-var fxml_url = 'http://flightxml.flightaware.com/json/FlightXML2/';
-var username = 'jasonandrewhoffman';
-var apiKey = '323da49e858c3d25fa589d1f6de62a96807f9dab';
-
-var origin = 'KIAD';
-var destination = 'KORD';
-
-
-rest.get(fxml_url + 'RoutesBetweenAirports', {
-	username: username,
-	password: apiKey,
-	query: {origin: origin, destination: destination}
-}).on('success', function(result, resp) {
-	console.log('Filed routes between ' + origin + ' and ' + destination + ':');
-	var r = result.RoutesBetweenAirportsResult.data;
-	for (var i = 0; i < r.length; i++) {
-		console.log('Route: ' + r[i].route + '\n' +
-			'Altitude: ' + r[i].filedAltitude);
-		};
-	})
-
-	*/
