98f2174c5f8cdf524b88021e3512ca0ac6308e23
Ja5onHoffman <jasonandrewhoffman@gmail.com>
2014-04-24 21:19:47 -0400

    Login overlay with 'x' working

--- client/mRoute.js
+++ client/mRoute.js
@@ -54,11 +54,33 @@ Template.navBar.events({
 	}
 })
 
-Template.overlay.events({
-	"click .overlay": function(e) {
+Template.loginForm.events({
+	"click .close": function(e) {
 		e.preventDefault;
 		Session.set('overlay', false);
-	}
+	},
+
+	"submit submit": function(e, t) {
+      e.preventDefault();
+      // retrieve the input field values
+      var email = t.find('#login-email').value;
+      var password = t.find('#login-password').value;
+
+        // Trim and validate your fields here.... 
+
+        // If validation passes, supply the appropriate fields to the
+        // Meteor.loginWithPassword() function.
+        Meteor.loginWithPassword(email, password, function(err){
+        if (err) {
+          // The user might not have been found, or their passwword
+          // could be incorrect. Inform the user that their
+          // login attempt has failed. 
+        } else {
+          // The user has been logged in.
+        }
+      });
+         return false; 
+      }
 })
 
 Template.overlay.helpers({
